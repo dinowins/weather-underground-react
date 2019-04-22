c35cd20f94a5cee39e8213a943e10ee56b1d2bbf
Dino Nguyen <dinodnguyen@gmail.com>
2019-04-21 17:50:20 -0700

    Add content and search bar(not functioning) to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../scss/styles.scss';
+import LogoWithName from '../assets/images/logo-with-name.png';
 
 function Header(){
   return (
-    <div>
-      <h1 className='bg-grn header'>Weather Underground</h1>
-      <Link to="/">Home</Link>
+    <div className='header'>
+      <img src={LogoWithName}></img>
+      <p id='sensor'>Sensor Network</p>
+      <p id='maps'>Maps & Radar</p>
+      <p id='severe'>Severe Weather</p>
+      <p id='news'>News & Blogs</p>
+      <p id='mobile'>Mobile Apps</p>
+      <p id='more'>More</p>
+      <input type='text' class='location-bar' placeholder='Search Locations'/>
+      <p className='membership'>Log In</p>
+      <p className='membership'>Join</p>
     </div>
   );
 }
