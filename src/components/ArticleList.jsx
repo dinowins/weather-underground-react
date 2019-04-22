69bf6a859c7984e7ace9c6f03e13c36dae721e5d
Dino Nguyen <dinodnguyen@gmail.com>
2019-04-21 17:36:37 -0700

    Add articleData to models and import to ArticleList

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,39 +1,10 @@
 import React from 'react';
 import Article from './Article';
-import stewie from '../assets/images/stewie.png';
-
-const masterArticleList = [
-  {
-    title: 'Storm Off the East Coast Has Undergone Bombogenesis',
-    description: 'An area of low pressure has rapidly strengthened off the East Coast and become a bomb cyclone.',
-    link: 'https://weather.com/forecast/regional/news/2019-04-01-northeast-storm-rain-wind-april-bomb-cyclone?cm_ven=wu_videos?cm_ven=hp-slot-1',
-  },
-  {
-    title: 'Billion-Dollar Flood',
-    description: 'Weeks of heavy rain and flooding in Iran have taken at least 62 lives.',
-    link: 'https://www.wunderground.com/cat6/Record-Floods-Iran-Kill-62-Cause-Over-1-Billion-Damage',
-  },
-  {
-    title: 'Severe Threat Update: Multi-Day, Multi-State',
-    description: 'A few tornadoes are possible.',
-    link: 'https://weather.com/storms/severe/news/2019-04-01-severe-thunderstorms-forecast-south-plains-mississippi-valley?cm_ven=wu_videos?cm_ven=hp-slot-3',
-  },
-  {
-    title: 'Hothouse Earth in Our Feature?',
-    description: 'Jeff Masters looks at the long-term effects of greenhouse emissions.',
-    link: 'https://www.wunderground.com/cat6/Global-CO2-Emissions-Hit-All-Time-High-2018-Hothouse-Earth-our-Future?cm_ven=hp-slot-4',
-  },
-  {
-    title: 'How the Sun is Impacting Our Climate',
-    description: 'Yes, the sun provides our heat. But no, the sun is not causing climate change.',
-    link: 'http://bit.ly/2HdLSBj?cm_ven=hp-slot-5',
-  }
-];
+import masterArticleList from '../models/articleData.js'
 
 function ArticleList(){
   return (
     <div>
-      <img src={stewie}></img>
       <hr/>
       {masterArticleList.map((article, index) =>
         <Article title={article.title}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import '../scss/styles.scss';
 function Header(){
   return (
     <div>
-      <h1 className='bg-grn header'>Help Queue!</h1>
+      <h1 className='bg-grn header'>Weather Underground</h1>
       <Link to="/">Home</Link>
     </div>
   );
