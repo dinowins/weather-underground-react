f742db8dd7617744802be5b45aee648fba80fcaf
Dino Nguyen <dinodnguyen@gmail.com>
2019-04-25 12:58:31 -0700

    Add hover effect to header links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,12 +7,12 @@ function Header(){
   return (
     <div className='header'>
       <Link to="/"><img src={LogoWithName}></img></Link>
-      <p id='sensor'>Sensor Network</p>
-      <p id='maps'>Maps & Radar</p>
-      <p id='severe'>Severe Weather</p>
-      <p id='news'>News & Blogs</p>
-      <p id='mobile'>Mobile Apps</p>
-      <p id='more'>More</p>
+      <p className='links' id='sensor'>Sensor Network</p>
+      <p className='links' id='maps'>Maps & Radar</p>
+      <p className='links' id='severe'>Severe Weather</p>
+      <p className='links' id='news'>News & Blogs</p>
+      <p className='links' id='mobile'>Mobile Apps</p>
+      <p className='links' id='more'>More</p>
       <input type='text' class='location-bar' placeholder='Search Locations'/>
       <Link to="/login"><p className='membership'>Log In</p></Link>
       <Link to="/join"><p className='membership'>Join</p></Link>
