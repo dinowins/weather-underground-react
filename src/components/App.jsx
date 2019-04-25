187f4e28ab802d7cf51be790e519795ae7747d2a
Dino Nguyen <dinodnguyen@gmail.com>
2019-04-25 12:24:06 -0700

    Remove api call for refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,30 +8,8 @@ import Footer from './Footer';
 import { Switch, Route } from 'react-router-dom';
 import Error404 from './Error404';
 
-class App extends React.Component{
+function App() {
 
-  constructor(props){
-    super(props);
-    this.state = {
-      weather: {
-        atlanta: null,
-        sanFrancisco: null
-      }
-    }
-    this.fetchWeather = this.fetchWeather.bind(this);
-  }
-
-  fetchWeather() {
-    fetch('https://api.darksky.net/forecast/e9181c11e82f5e3ecfc0d3d0bfb71525/37.8267,-122.4233')
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(myJson) {
-        console.log(JSON.stringify(myJson));
-      });
-  }
-
-  render(){
     return (
       <div>
       <Header/>
@@ -46,7 +24,6 @@ class App extends React.Component{
       <Footer/>
       </div>
     );
-  }
 }
 
 export default App;
