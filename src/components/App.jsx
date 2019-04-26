9a06cd53824d7589ee00a6562f09c4a8ae4dac7e
Dino Nguyen <dinodnguyen@gmail.com>
2019-04-26 13:39:19 -0700

    Show temperature to weathermast

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,12 +8,36 @@ import Footer from './Footer';
 import { Switch, Route } from 'react-router-dom';
 import Error404 from './Error404';
 
-function App() {
+class App extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      cityList: {
+        SF: {
+          name: "San Francisco",
+          temp: 70
+        },
+        ATL: {
+          name: "Atlanta",
+          temp: 84
+        }
+      }
+    }
+    this.changeTemp = this.changeTemp.bind(this);
+  }
+
+
+  changeTemp() {
+      console.log(this.state.cityList);
+  }
+
+  render(){
     return (
       <div>
       <Header/>
-      <WeatherMast/>
+      <button onClick={this.changeTemp}>Change temp</button>
+      <WeatherMast NewCities = {this.state.cityList}/>
       <Switch>
       <Route exact path='/' component={ArticleList} />
       <Route exact path='/join' component={Join} />
@@ -24,6 +48,7 @@ function App() {
       <Footer/>
       </div>
     );
+  }
 }
 
 export default App;
--- src/components/WeatherMast.jsx
+++ src/components/WeatherMast.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../scss/styles.scss';
+import PropTypes from "prop-types";
 
-function WeatherMast() {
+function WeatherMast(props) {
   return (
     <div className='favorites-bar'>
       <p id='popular-cities'>Popular Cities</p>
       <p className='city-link'><a href='https://www.wunderground.com/weather/us/ca/san-francisco/37.77%2C-122.42'>San Francisco, CA</a></p>
+      <h1>{props.NewCities.SF.temp}</h1>
       <p className='city-link'><a href='https://www.wunderground.com/weather/us/ny/manhattan/40.71,-74.01'>Manhattan, NY</a></p>
       <p className='city-link'><a href='https://www.wunderground.com/weather/us/ga/atlanta/33.75,-84.39'>Atlanta, GA</a></p>
       <p className='city-link'><a href='https://www.wunderground.com/weather/us/il/chicago/41.88,-87.63'>Chicago, IL</a></p>
       <p className='city-link'><a href='https://www.wunderground.com/weather/us/ma/boston/42.36,-71.06'>Boston, MA</a></p>
     </div>
   );
+}
+
+WeatherMast.propTypes = {
+  NewCities: PropTypes.object
 }
 
 export default WeatherMast;
