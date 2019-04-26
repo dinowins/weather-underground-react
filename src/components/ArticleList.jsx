2414340f8f581c49a4fbe36007cd4a4aca16c27a
Dino Nguyen <dinodnguyen@gmail.com>
2019-04-26 11:36:13 -0700

    Show conditions component in articleList component

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Article from './Article';
 import SignUpHero from './SignUpHero';
+import Conditions from './Conditions';
 import masterArticleList from '../models/articleData.js'
 
 function ArticleList(){
   return (
     <div>
+      <Conditions/>
       <SignUpHero/>
       {masterArticleList.map((article, index) =>
         <Article title={article.title}
