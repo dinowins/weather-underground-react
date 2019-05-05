1286792e3636d802fdbdfbcd4cc43bbaa24f2796
Dino Nguyen <dino@>
2019-05-05 15:54:58 -0700

    Add City jsx file

---
+++ src/components/City.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../scss/styles.scss';
+import PropTypes from 'prop-types';
+
+function City(props) {
+  return (
+    <p className='city-link'><a href={props.link}>{props.temp}°F {props.name}</a></p>
+  )
+}
+
+City.propTypes = {
+  name: PropTypes.string.isRequired,
+  temp: PropTypes.number.isRequired,
+  link: PropTypes.string.isRequired
+}
+
+export default City; 
